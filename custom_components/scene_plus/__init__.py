bb10d387a13e8e46a43756e70bca4ad099d72aab
Tom Bruno <tom.bruno@smartqasa.com>
2026-01-07 14:16:50 -0500

    refactor: update integration setup logging and manifest configuration

--- custom_components/scene_plus/__init__.py
+++ custom_components/scene_plus/__init__.py
@@ -1,9 +1,18 @@
 from homeassistant.core import HomeAssistant
-
+from .const import DOMAIN
 from .services_scene import register_scene_services
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up the scene_plus integration."""
+
+    _LOGGER.info("Setting up %s integration", DOMAIN)
+
+    hass.data.setdefault(DOMAIN, {})
+
     register_scene_services(hass)
+
     return True
